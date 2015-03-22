22a3e5d0aeb3891f0de7f85b7f0c01d16f88cd2e
RNPAnalytics <naidupotturu@gmail.com>
2015-03-21 17:02:23 -0700

    uploading files for R ProgrammingAssingment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
 ## Put comments here that give an overall description of what your
+
 ## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Create a "CacheVector" that will cache and  calculate  its inverse
+makeCacheMatrix <- function(matrix=m,row=2,col=2) {
+  m1 <- NULL
+  
+  m <<- matrix(m,nrow=row,ncol=col) 
+  m1 <<- NULL
+  print(m)
+  
+  ## create a cacheSolve function that returns the inverse of a matrix,it if necessary/existing simply display
+  cacheSolve <- function() {
+    
+    
+    if (is.null(m1)) {
+      m1 <<- solve(m)
+      print(m)
+    }
+    m1
+  }
+  ## return the CacheVector object as a list of function
+  list(cacheSolve = cacheSolve)
 }
