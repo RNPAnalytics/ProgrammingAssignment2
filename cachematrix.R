6c0ec6ab506bbd12238c2dd663addb63b0891afc
RNPAnalytics <naidupotturu@gmail.com>
2015-03-22 11:05:22 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,18 @@
 ## functions do
 ## Create a "CacheVector" that will cache and  calculate  its inverse
 makeCacheMatrix <- function(m=matrix()) {
-  m1 <<- NULL
+  str <<-formals(m)
   print(m)
   
   ## create a cacheSolve function that returns the inverse of a matrix,it if necessary/existing simply display
   cacheSolve <- function() {
-     m1 <<- solve(t(m) %*% m)
-     return(m1)
-    }
-  ## return the CacheVector object as a list of function
+  if(str == "NULL") return(m1)
+  else
+  {
+  m1 <<- NULL
+  m1 <<- solve(t(m) %*% m)
+  return(m1)
+  }
+  return the CacheVector object as a list of function
   list(cacheSolve = cacheSolve)
 }
